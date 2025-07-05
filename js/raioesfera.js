fe54a3097cb6732be124a2577868ef8c8a671efa
edoardo1974 <edoardo.mozzato@gmail.com>
2025-07-05 12:53:04 +0100

    Last Version

--- js/raioesfera.js
+++ js/raioesfera.js
@@ -4,11 +4,10 @@
         let volume = (4 / 3) * Math.PI * raio;
         return volume;
     }
+    // This function calculates the volume of a sphere based on the radius provided by the user
+
     let numero = prompt("Inserisci un numero:");
     let formula = raioEsfera(numero);
-    console.log('O volume da esfera é:'+ raioEsfera(numero));
-    // Define a função raioEsfera que calcula o volume de uma esfera
-    // com base no raio fornecido pelo usuário
+    console.log('O volume da esfera é:'+ raioEsfera(numero));// This line calculates the volume of the sphere using the raioEsfera function and logs it to the console
     console.log(`O volume da esfera é igual a ${formula} portanto uma esfera de raio ${numero} tem um volume de ${formula}`);
-    // Chama a função com o número inserido pelo usuário
-    // e exibe o resultado no console
+    
